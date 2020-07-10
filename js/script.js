6bdb77b0d7165c9f47d3e7577500b9985cee4445
Cristian Corrales <cristiancorrales84@gmail.com>
2020-07-10 00:09:33 -0400

    Created random Color function

--- js/script.js
+++ js/script.js
@@ -8,12 +8,16 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * `quotes` array
+ * 
+ * An array of Objects that hold quotes, and source.
+ * Some objects hold citation and some hold year properties and values 
 ***/
   let quotes = [
     {
       quote: "We Generate Fears While We Sit. We Overcome Them By Action.",
-      source: "Dr. Henry Link"
+      source: "Dr. Henry Link",
+      tags: "inspirational, life, challenge"
 
     },
     {
@@ -47,44 +51,101 @@ project 1 - A Random Quote Generator
 
 /***
  * `getRandomQuote` function
+ *  Gets a random quote from the quotes array
+ * 
+ * @returns {object} The randomized selected quote
 ***/
   const getRandomQuote = () =>{
+    //Created a variable to store a random number from 0 to 4(The index of last object in array) 
     const randNum = Math.floor(Math.random() * quotes.length );
+    //Logs the random Number
     console.log(randNum);
-    console.log(randNum);
+    //Returns a random selected quote object within the quotes array
     return quotes[randNum];
 
   }
-
+  //Calls the getRandomQuote function
   getRandomQuote();
+
+
+  /***
+ * `getRandomColor` function
+ *  Creates a random rgb value 
+ * 
+ * @returns {string} The randomized rgb value
+***/
+  const getRandomColor = () =>{
+    //New string variable that starts off the rgb value
+    let rgb = "rgb(";
+    //Outer for loop that loops 1 time
+    for(let i = 0; i < 1; i++){
+      //Inner for loop that loops 2 times
+      for(let index = 0; index < 3; index++){
+        //Adds a randomized number from 0 to 255 and adds it to the rgb variable along with a comma at the end
+        rgb+= `${Math.floor(Math.random() * 256)},`;
+      }
+      //Adds a randomized number from 0 to 255 and adds it to the rgb variable
+      rgb+= `${Math.floor(Math.random() * 256)})`;
+    }
+    //Returns the randomized string of RGB values
+    return rgb;
+  }
+  
 
 /***
  * `printQuote` function
+ *  Creates and outputs the random quote in the HTML
+ * 
 ***/
   const printQuote = () =>{
+    
+    //Calls getRandomQuote function and sets it to a new variable randQuote which will hold the random object
     const randQuote = getRandomQuote();
-    quoteHTML = `<p class="quote">${randQuote.quote}</p>
+    
+    //Created a variable that will hold the HTML and started adding the paragraph tags to display the quote
+    let quoteHTML = `<p class="quote">${randQuote.quote}</p>
     <p class="source">${randQuote.source}`;
-
+    
+    //Conditional that uses hasOwnProperty method to determine if citation is a property in the random quote.
+    //Will return True or False
     if(randQuote.hasOwnProperty("citation")){
-      quote+= `<span class="citation">${quoteHTML.citation}</span>`;
+      //If true, concatenate a span tag to the string of HTML that will display the citation property
+      quoteHTML += `<span class="citation">${randQuote.citation}</span>`;
 
     }
 
+    //Conditional that uses hasOwnProperty method to determine if year is a property in the random quote.
     if(randQuote.hasOwnProperty("year")){
+      //If true, concatenate a span tag to the string of HTML that will display the year property
       quoteHTML+= `<span class="year">${randQuote.year}</span>`;
     }
+
+    //Concatenate closing p tag
     quoteHTML+= "</p>";
 
+    if(randQuote.hasOwnProperty("tags")){
+
+      quoteHTML+= `<p class = "tags">Tags: ${randQuote.tags}</p>`
+
+    }
+
+
+    //Grabs element by ID= quote-box and displays the quote within the html
+    document.getElementById('quote-box').innerHTML = quoteHTML;
+
+    document.querySelector("body").style.backgroundColor = getRandomColor();
+    
+    //returns the string of HTML that displays the quote
     return quoteHTML;
   }
-
+  
+  
 
 
 
 /***
  * click event listener for the print quote button
- * DO NOT CHANGE THE CODErr BELOW!!
+ * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
